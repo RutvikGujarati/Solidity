6d4ee296e3d70c0c741334fffc7e3c8cfd60f541
Rutvik Gujarati <125241992+RutvikGujarati@users.noreply.github.com>
2023-08-14 02:04:40 +0530

    Update FlashLoan.sol

--- Learning-phase/FlashLoan.sol
+++ Learning-phase/FlashLoan.sol
@@ -1,20 +1,47 @@
-
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.10;
 
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IPoolAddressesProvider.sol";
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
-contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
+contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable owner;
 
     constructor(address _addressProvider)
         FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
     {
+        owner = payable(msg.sender);
     }
 
-    function fn_RequestFlashLoan(address _token, uint256 _amount) public {
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        //
+        // This contract now has the funds requested.
+        // Your logic goes here.
+        //
+
+        // At the end of your logic above, this contract owes
+        // the flashloaned amount + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the Pool contract allowance to *pull* the owed amount
+        uint256 amountOwed = amount + premium;
+        IERC20(asset).approve(address(POOL), amountOwed);
+
+        return true;
+    }
+
+    function requestFlashLoan(address _token, uint256 _amount) public {
         address receiverAddress = address(this);
         address asset = _token;
         uint256 amount = _amount;
@@ -29,23 +56,22 @@ contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
             referralCode
         );
     }
-    
-        //This function is called after your contract has received the flash loaned amount
 
-    function  executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-    )  external override returns (bool) {
-        
-        //Logic goes here
-        
-        uint256 totalAmount = amount + premium;
-        IERC20(asset).approve(address(POOL), totalAmount);
+    function getBalance(address _tokenAddress) external view returns (uint256) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
 
-        return true;
+    function withdraw(address _tokenAddress) external onlyOwner {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function"
+        );
+        _;
     }
 
     receive() external payable {}
